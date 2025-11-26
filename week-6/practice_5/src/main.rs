ebb7f81eb0d11137190077422eaa69398bbfd779
elumahfrederick-coder <elumahfrederick@gmail.com>
2025-11-26 13:34:45 +0100

    (my first fnx commit)

---
+++ week-6/practice_5/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let fullname = " Pan-Atlantic University ";
+    println!();
+    println!("Name: {}", fullname);
+    println!();
+    println!("Before trim ");
+    println!("length is {}",fullname.len());
+    
+}
