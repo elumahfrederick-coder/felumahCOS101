300a729a08d25a5262fdd943c68cb9077243a10a
elumahfrederick-coder <elumahfrederick@gmail.com>
2025-11-27 15:23:15 +0100

    (week 3 commit)

--- week-9/practice_1/src/main.rs
+++ week-9/practice_1/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let dept = " Department of Computer Science";
 
     let mut file = std::fs::File::create("data.txt").expect("create failed");
-    file.write_all("Welcome to Ryst Programming\n"
+    file.write_all("Welcome to Rust Programming\n"
         .as_bytes()).expect("write failed");
     file.write_all(announce.as_bytes()).expect("write failed");
     file.write_all(dept.as_bytes()).expect("write failed");
